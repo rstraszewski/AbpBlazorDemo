229b7771b35f5030fa034c509a9901f061378ec0
r.straszewski <rstraszewski@gmail.com>
2023-01-15 22:16:55 +0100

    Init Abp

---
+++ src/Acme.BookStore.Application.Contracts/CreateUpdateBookDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Acme.BookStore
+{
+    public class CreateUpdateBookDto
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        public BookTypeDto Type { get; set; } = BookTypeDto.Undefined;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public float Price { get; set; }
+    }
+}
